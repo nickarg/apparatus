15e0cec3fa53314a92ce81e18fafb43c4eb11a10
Nikos Argyropoulos <nikos.rgrpls@gmail.com>
2017-06-27 15:30:53 +0100

    Sectro Shapes

--- src/addEdge.js
+++ src/addEdge.js
@@ -2,14 +2,61 @@
 
 const printChat = require('./printChat.js')
 
-module.exports = function addComponent (cy, sourceNode, targetNode) {
-  cy.add({
-    group: 'edges',
-    data: {
-      id: `${sourceNode}${targetNode}`,
-      source: `${sourceNode}`,
-      target: `${targetNode}`
-    }
-  })
+module.exports = function addComponent (cy, sourceNode, targetNode, sourceNodeConcept, targetNodeConcept) {
+  if (sourceNodeConcept === 'goal' || sourceNodeConcept === 'plan') {
+      if (targetNodeConcept === 'resource') {
+        cy.add({
+          group: 'edges',
+          data: {
+            id: `${sourceNode}${targetNode}`,
+            source: `${sourceNode}`,
+            target: `${targetNode}`
+          }
+        }).addClass('creates')
+      }
+}
+
+      if (sourceNodeConcept ==='constraint') {
+        if (targetNodeConcept === 'goal' || targetNodeConcept === 'plan' || targetNodeConcept === 'resource') {
+          cy.add({
+            group: 'edges',
+            data: {
+              id: `${sourceNode}${targetNode}`,
+              source: `${sourceNode}`,
+              target: `${targetNode}`
+            }
+          }).addClass('restricts')
+        }
+}
+
+        // default:
+        //   cy.add({
+        //     group: 'edges',
+        //     data: {
+        //       id: `${sourceNode}${targetNode}`,
+        //       source: `${sourceNode}`,
+        //       target: `${targetNode}`
+        //     }
+        //   })
+        // }
+
+  // if (sourceNodeConcept === 'data' && targetNodeConcept === 'asset') {
+  //   cy.add({
+  //     group: 'edges',
+  //     data: {
+  //       id: `${sourceNode}${targetNode}`,
+  //       source: `${sourceNode}`,
+  //       target: `${targetNode}`
+  //     }
+  //   }).addClass('test-edge')
+  // }
+  // cy.add({
+  //   group: 'edges',
+  //   data: {
+  //     id: `${sourceNode}${targetNode}`,
+  //     source: `${sourceNode}`,
+  //     target: `${targetNode}`
+  //   }
+  // })
   printChat('edge added')
 }
--- style/graphStyle.js
+++ style/graphStyle.js
@@ -5,52 +5,134 @@ const configGraph = require(`./configGraph.js`)
 const graphStyle = {}
 
 graphStyle.style = [{
-  selector: 'node',
-  style: {
-    shape: 'ellipse',
-    'background-color': configGraph.gray,
-    label: 'data(label)',
-    'text-valign': 'center',
-    'color': configGraph.white,
-    'text-outline-width': 2,
-    'text-outline-color': configGraph.black
-  }
-}, {
-  selector: 'edge',
-  style: {
-    'curve-style': 'bezier',
-    'line-color': configGraph.gray,
-    // label: 'data(label)',
-    'target-arrow-shape': 'triangle',
-    'target-arrow-color': configGraph.gray,
-    'width': 2,
-    'color': configGraph.white,
-    'text-outline-width': 2,
-    'text-outline-color': configGraph.black
-  }
-}, {
-  selector: '.faded',
-  style: {
-    'opacity': 0.25,
-    'text-opacity': 0
-  }
-}, {
-  selector: '.selection',
-  style: {
-    'background-color': configGraph.blue,
-    'line-color': configGraph.blue,
-    label: 'data(label)'
-  }
-}, {
-  selector: '.attention',
-  style: {
-    'background-color': configGraph.yellow
-  }
-}, {
-  selector: '.protect',
-  style: {
-    'background-color': configGraph.lightBlue
-  }
-}]
+    selector: 'node',
+    style: {
+      // shape: 'ellipse',
+      'background-color': configGraph.gray,
+      label: 'data(label)',
+      'text-valign': 'center',
+      'color': configGraph.white,
+      'text-outline-width': 2,
+      'text-outline-color': configGraph.black
+    }
+  }, {
+    selector: 'edge',
+    style: {
+      'curve-style': 'bezier',
+      'line-color': configGraph.gray,
+      label: 'data(label)',
+      'mid-target-arrow-shape': 'triangle',
+      'mid-target-arrow-color': configGraph.gray,
+      'width': 2,
+      'color': configGraph.white,
+      'text-outline-width': 2,
+      'text-outline-color': configGraph.black,
+      'source-text-offset': 1
+    }
+  }, {
+    selector: '.faded',
+    style: {
+      'opacity': 0.25,
+      'text-opacity': 0
+    }
+  }, {
+    selector: '.selection',
+    style: {
+      'background-color': configGraph.blue,
+      'line-color': configGraph.blue,
+      label: 'data(label)'
+    }
+  }, {
+    selector: '.attention',
+    style: {
+      'background-color': configGraph.yellow
+    }
+  }, {
+    selector: '.protect',
+    style: {
+      'background-color': configGraph.lightBlue
+    }
+  }, {
+    selector: '.actor',
+    style: {
+      shape: 'ellipse',
+      'background-color': configGraph.magenta
+    }
+  }, {
+    selector: '.goal',
+    style: {
+      shape: 'roundrectangle',
+      'background-color': configGraph.green
+    }
+  }, {
+    selector: '.plan',
+    style: {
+      shape: 'vee',
+      'background-color': configGraph.blue
+    }
+  }, {
+    selector: '.resource',
+    style: {
+      shape: 'rectangle',
+      'background-color': configGraph.yellow
+    }
+  }, {
+    selector: '.mechanism',
+    style: {
+      shape: 'rhomboid',
+      'background-color': configGraph.green
+    }
+  }, {
+    selector: '.constraint',
+    style: {
+      shape: 'octagon',
+      'background-color': configGraph.red
+    }
+  }, {
+    selector: '.threat',
+    style: {
+      shape: 'triangle',
+      'background-color': configGraph.orange
+    }
+  }, {
+    selector: '.and',
+    style: {
+      shape: 'diamond',
+      'background-color': configGraph.blue
+    }
+  }, {
+    selector: '.or',
+    style: {
+      shape: 'diamond',
+      'background-color': configGraph.blue
+    }
+  },
+  // declare edge shapes
+  {
+    selector: '.creates',
+    style: {
+      //'line-style': 'dashed'
+      label: 'creates'
+    }
+  },
+  {
+    selector: '.requires',
+    style: {
+      //'line-style': 'dashed'
+      label: 'requires'
+    }
+
+  }, {
+    selector: '.restricts',
+    style: {
+      //'line-style': 'dashed'
+       label: 'restricts',
+      // 'text-valign': 'bottom',
+      // 'source-arrow-color': configGraph.gray,
+      // 'source-arrow-shape': 'triangle',
+      // 'source-arrow-fill': 'filled'
+    }
+  },
+]
 
 module.exports = graphStyle
